d5d025a3815173ee18cac369b4e203efb2f423ab
Salaheddine <hzsalaheddine@gmail.com>
2020-12-10 16:59:08 +0100

    Earth

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,41 @@
-// earth
+earth
 
-// var earth;
-// function initialize() {
-//   earth = new WE.map('earth_div');
-//   earth.setView([48.801408, 2.130122], 1);
-//   WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-//     attribution: '© OpenStreetMap contributors'
-//   }).addTo(earth);
+var earth;
+function initialize() {
+  earth = new WE.map('earth_div');
+  earth.setView([48.801408, 2.130122], 2);
+  WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(earth);
 
-//   // Start a simple rotation animation
-//   var before = null;
-//   requestAnimationFrame(function animate(now) {
-//       var c = earth.getPosition();
-//       var elapsed = before? now - before: 0;
-//       before = now;
-//       earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
-//       requestAnimationFrame(animate);
-//   });
-// }
+  // Start a simple rotation animation
+  var before = null;
+  requestAnimationFrame(function animate(now) {
+      var c = earth.getPosition();
+      var elapsed = before? now - before: 0;
+      before = now;
+      earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+      requestAnimationFrame(animate);
+  });
+  var marker = WE.marker([41.3833, 2.1833]).addTo(earth);
+  marker.bindPopup("<b>Voyagaez à </b><a href='https://fr.wikipedia.org/wiki/Barcelone'>Barcelone</a><span style='font-size:10px;color:#999'></span>", {maxWidth: 150, closeButton: true})//.openPopup();
+
+  var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
+  marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: true});
+
+  var marker3 = WE.marker([48.801408, 2.130122]).addTo(earth);
+  marker3.bindPopup("<b>Versailles</b><br>Meilleure Ville du monde!", {maxWidth: 120, closeButton: false});
+
+  var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+
+
+}
+
+
+
+
+
+  
+
+
+
