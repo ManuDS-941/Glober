84b1257c25cb1c09b1ea4f81c9b622565109657b
Salaheddine <hzsalaheddine@gmail.com>
2020-12-10 10:14:45 +0100

    EsController + path navbar

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+// earth
+
+// var earth;
+// function initialize() {
+//   earth = new WE.map('earth_div');
+//   earth.setView([48.801408, 2.130122], 1);
+//   WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+//     attribution: '© OpenStreetMap contributors'
+//   }).addTo(earth);
+
+//   // Start a simple rotation animation
+//   var before = null;
+//   requestAnimationFrame(function animate(now) {
+//       var c = earth.getPosition();
+//       var elapsed = before? now - before: 0;
+//       before = now;
+//       earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+//       requestAnimationFrame(animate);
+//   });
+// }
